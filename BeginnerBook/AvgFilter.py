a5f886802d01705aea83d3640d99b1994c3a80ba
Fredrick Stakem <fstakem@fstakem-main.local>
2013-03-28 21:22:03 -0400

    Redid the code in a more pythonic fashion and finished the unit tests.

---
+++ BeginnerBook/AvgFilter.py
@@ -0,0 +1,53 @@
+# ------------------------------------------------------
+#
+#   AvgFilter.py
+#   By: Fred Stakem
+#   Created: 3.28.13
+#
+# ------------------------------------------------------
+
+# The following code is an adaptation from the book
+# "Kalman Filter for Beginners with MATLAB Examples"
+# By Phil Kim.
+
+# Libs
+
+# Locals
+import Utilities
+
+class AvgFilter(object):
+    
+    # Class methods/variables
+    Utilities.getLogger('AverageFilter')
+    
+    def __init__(self, init_values=[]):
+        self.previous_mean = 0
+        self.num_samples = 0
+        
+        if len(init_values) > 0:
+            for x in init_values:
+                self.calculateNextAverage(x)
+    
+    def __call__(self, x, reset=False):
+        if reset:
+            self.reset()
+        
+        return self.calculateNextAverage(x)
+    
+    def reset(self):
+        self.previous_mean = 0
+        self.num_samples = 0
+    
+    def calculateNextAverage(self, x):
+        new_num_samples = self.num_samples + 1
+        alpha = (new_num_samples - 1) / float(new_num_samples)
+        average = alpha * self.previous_mean + (1 - alpha) * x
+        
+        self.num_samples = new_num_samples
+        self.previous_mean = average
+        
+        return average
+    
+    
+    
+
---
+++ BeginnerBook/TestAvgFilter.py
@@ -0,0 +1,97 @@
+# ------------------------------------------------------
+#
+#   TestAvgFilter.py
+#   By: Fred Stakem
+#   Created: 3.28.13
+#
+# ------------------------------------------------------
+
+# Libs
+import unittest
+import random
+import matplotlib.pyplot as plt
+import Globals as globals
+from Utilities import *
+from AvgFilter import AvgFilter
+
+class AvgFilterTest(unittest.TestCase):
+    
+    # Setup logging
+    logger = getLogger('AvgFilterTest')
+    graph_file = '../output/RandomAvgFilter.png'
+    
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+        
+    @log_test(logger, globals.log_separator)
+    def testAvgFilterInit(self):
+        init_data = [10, 20, 30, 40, 50]
+        expected_mean = 30
+        
+        filter = AvgFilter(init_data)
+        mean = filter.previous_mean
+        
+        output = 'Initial data: %s Expected mean: %s Actual mean: %s' % (str(init_data), str(expected_mean), str(mean))
+        AvgFilterTest.logger.debug(output)
+        assert mean == expected_mean, 'AvgFilter class was incorrectly initialized.'
+    
+    @log_test(logger, globals.log_separator)
+    def testAvgFilterReset(self):
+        init_data = [10, 20, 30, 40, 50]
+        added_value = 15
+        expected_mean = added_value
+        
+        filter = AvgFilter(init_data)
+        mean = filter(added_value, True)
+        
+        output = 'Additional data: %s Expected mean: %s Actual mean: %s' % (str(added_value), str(added_value), str(expected_mean))
+        AvgFilterTest.logger.debug(output)
+        assert mean == expected_mean, 'AvgFilter class was incorrectly reset.'
+    
+    @log_test(logger, globals.log_separator)
+    def testAvgFilter(self):
+        test_data = [10, 20, 30, 40, 50]
+        expected_means = [10, 15, 20, 25, 30]
+        filter = AvgFilter()
+        
+        AvgFilterTest.logger.debug('Initial mean: %f' % (filter.previous_mean))
+        for i, x in enumerate(test_data):
+            mean = filter(x)
+            output = 'Additional data: %s Expected mean: %s Actual mean: %s' % (str(x), str(expected_means[i]), str(mean))
+            AvgFilterTest.logger.debug(output)
+            assert mean == expected_means[i], 'AvgFilter class filtered incorrectly.'
+        
+    @log_test(logger, globals.log_separator)
+    def testAvgFilterGraphically(self):
+        test_data = self.generateRandomSignal(50)
+        time = range(0, len(test_data) * 10, 10)
+        filtered_data = []
+        filter = AvgFilter()
+        
+        for x in test_data:
+            filtered_data.append( filter(x) )
+            
+        fig = plt.figure()
+        subplot = fig.add_subplot(111)
+        subplot.plot(time, test_data, 'o-')
+        subplot.plot(time, filtered_data, 'ko-')
+        subplot.set_xlabel('Time (s)')
+        subplot.set_ylabel('Voltage (V)')
+        
+        plt.savefig(AvgFilterTest.graph_file)
+    
+    
+    def generateRandomSignal(self, size):
+        x = []
+        
+        for y in range(size):
+            x.append( random.uniform(10, 20) )
+            
+        return x
+        
+    
+    
+    
